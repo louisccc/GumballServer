2624abe83e8edd6c61f999701948ef2c7dbe27c2
YU SHIHYUAN <yushihyuan@louisccc.local>
2012-08-08 16:51:13 -0700

    revise feedback api

--- php/getFeedbackStatus.php
+++ php/getFeedbackStatus.php
@@ -2,8 +2,6 @@
 require_once("Dbhelper.php");
 
 $db = new DB();
-//$id = $_GET['id'];
-//$db->updateFeedbackStatusBy($id);
 if(isset($_GET['device_id'])){
     $dev_id = $_GET['device_id'];
     $result = $db->getFeedbackStatusBy($dev_id);
--- php/insertFeedback.php
+++ php/insertFeedback.php
@@ -2,7 +2,13 @@
 require_once("Dbhelper.php");
 if(isset($_GET['application_id'])){
     $app_id = $_GET['application_id'];
+    $type = $_GET['type'];
     $db = new DB();
-    $db->insertFeedbackStatusBy($_SERVER['REMOTE_ADDR'], $app_id,"positive");
+    if($type == "positive"){
+        $db->insertFeedbackStatusBy($_SERVER['REMOTE_ADDR'], $app_id,"positive");
+    }
+    else if($type == "negative"){
+        $db->insertFeedbackStatusBy($_SERVER['REMOTE_ADDR'], $app_id,"negative");
+    }
 }
 ?>
