24090e453bbb5c3a2dd2bc2a2574aba47f5902bd
YU SHIHYUAN <yushihyuan@louisccc.local>
2012-08-10 18:24:13 -0700

    buggy getOnlineList

--- php/getOnlineList.php
+++ php/getOnlineList.php
@@ -4,7 +4,7 @@ require_once("onlineUser.php");
 
 $db_help = new DB();
 refreshOnlineStatus($db_help);
-$resul = $db_help->getOnlineList();
+$result = $db_help->getOnlineList();
 echo json_encode($result);
 
 ?>
