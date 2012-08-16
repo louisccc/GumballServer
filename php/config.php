7989f33bb5ac51054512a18e2bddc73cc9aca992
YU SHIHYUAN <yushihyuan@unknown7cd1c3dd1d3f.gateway.2wire.net>
2012-08-16 13:15:48 -0700

    put on config.php

---
+++ php/config.php
@@ -0,0 +1,25 @@
+<?php
+
+$account = "root";
+$password = "29418401";
+
+class Config{
+	static $confArray;
+
+	public static function read($name)
+	{
+		return self::$confArray[$name];
+	}
+
+	public static function write($name, $value)
+	{
+		self::$confArray[$name] = $value;
+	}
+}
+
+Config::write('db.host', 'localhost');
+Config::write('db.basename', 'sweetfeedback');
+Config::write('db.user', 'root');
+Config::write('db.password', '29418401');
+
+?>
