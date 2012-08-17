70648bd59cb35e56a26d5bf1c8d8975ae2fba5cf
YU SHIHYUAN <yushihyuan@unknown7cd1c3dd1d3f.gateway.2wire.net>
2012-08-17 01:00:59 -0700

    debug in makeNewReport

--- php/makeNewReport.php
+++ php/makeNewReport.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db.php");
-$coming_ip = $_SERVER['REMOTE_ADDR']
+$coming_ip = $_SERVER['REMOTE_ADDR'];
 if(isset($_GET["user_id"]) && isset($_GET["coordinate_x"]) && isset($_GET["coordinate_y"]) && isset($_GET["title"])){
     $user_id = $_GET["user_id"];
     $coordinate_x = $_GET["coordinate_x"];
