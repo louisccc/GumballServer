afc61d13a0ff70559a63dcce12a961de9778db54
janetyc <janetyc@gmail.com>
2012-08-20 11:20:49 -0700

    integrate report fixing into new UI

--- php/makeFixReport.php
+++ php/makeFixReport.php
@@ -1,7 +1,9 @@
 <?php
+
 require_once("db.php");
 require_once("feedback_type.php");
 $coming_up = $_SERVER['REMOTE_ADDR'];
+
 if (isset($_GET["report_id"]) && isset($_GET["user_id"])) {
     $db_help = new DB();
     $report_id = $_GET["report_id"];
@@ -9,6 +11,5 @@ if (isset($_GET["report_id"]) && isset($_GET["user_id"])) {
     $db_help->updateFixReport($user_id, $report_id);
     $db_help->insertFeedbackStatusBy($coming_up, feedbackType::$makeFixReport, "positive");
 }
-
 
 ?>
--- php/makeNewReport.php
+++ php/makeNewReport.php
@@ -1,6 +1,8 @@
 <?php
+
 require_once("db.php");
 $coming_ip = $_SERVER['REMOTE_ADDR'];
+
 if(isset($_GET["user_id"]) && isset($_GET["coordinate_x"]) && isset($_GET["coordinate_y"]) && isset($_GET["title"])){
     $user_id = $_GET["user_id"];
     $coordinate_x = $_GET["coordinate_x"];
@@ -10,5 +12,4 @@ if(isset($_GET["user_id"]) && isset($_GET["coordinate_x"]) && isset($_GET["coord
     $db_help->insertFixReport($title, $coordinate_x, $coordinate_y, $user_id);
     $db_help->insertFeedbackStatusBy($coming_ip, 2, "positive");
 }
-
 ?>
