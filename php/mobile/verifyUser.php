0264baba6dea8e6e203152d0e876bcc029bd043f
janetyc <janetyc@gmail.com>
2012-08-28 17:07:44 -0700

    use session to check login status

--- php/mobile/verifyUser.php
+++ php/mobile/verifyUser.php
@@ -1,17 +1,50 @@
 <?php
+session_start();
 require_once("../db.php");
 require_once("../onlineUser.php");
 
-if( isset($_GET["account"]) && isset($_GET["password"])){
-    $username = $_GET["account"];
-    $password = $_GET["password"];
-    $db = new DB();
-    $result = $db->verifyUser($username, $password);
-    if($result!=null){
-        userLogin($db, $result["token"], $_SERVER["REMOTE_ADDR"]);
-        $result['success'] = 1;
+$inactive = 3*24*60*60; // 3 days
+
+if(isset($_SESSION['is_logged_in']) && ($_SESSION['is_logged_in'] == 1)){
+    $session_life = time() - $_SESSION['create_time'];
+
+    if($session_life > $inactive){
+        session_destroy();
+        $result['is_logged_in'] = 0;
+    }else{
+        $result['is_logged_in'] = 1;
+        $result['account'] = $_SESSION['account'];
+        $result['token'] = $_SESSION['token'];
+    }
+    echo json_encode($result);
+}else{
+
+    //login
+    if( isset($_GET["account"]) && isset($_GET["password"])){
+        $username = $_GET["account"];
+        $password = $_GET["password"];
+        $db = new DB();
+        $result = $db->verifyUser($username, $password);
+
+        if($result!=null){
+            userLogin($db, $result["token"], $_SERVER["REMOTE_ADDR"]);
+            $result['success'] = 1;
+            $result['is_logged_in'] = 1;
+            
+            $_SESSION['is_logged_in'] = 1;
+            $_SESSION['account'] = $username;
+            $_SESSION['token'] = $result["token"];
+            $_SESSION['create_time'] = time();
+
+        }else{
+            $result['success'] = 0;
+            $result['is_logged_in'] = 0;
+            $_SESSION['is_logged_in'] = 0;
+        }
     }else{
         $result['success'] = 0;
+        $result['is_logged_in'] = 0;
+        $_SESSION['is_logged_in'] = 0;
     }
     echo json_encode($result);
 }
