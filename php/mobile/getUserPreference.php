478aef5264e97cc4984ba12e1d8456a1380d1669
janetyc <janetyc@gmail.com>
2012-09-16 16:34:45 -0700

    let user can setup his/her preference when he/she log in the site

--- php/mobile/getUserPreference.php
+++ php/mobile/getUserPreference.php
@@ -1,14 +1,14 @@
 <?php
 require_once("../db.php");
 $db= new DB();
+$result['success'] = 0;
 if(isset($_GET['token'])){
     $token = $_GET['token'];
     $user_id = $db->getUserIdByToken($token);
     if($user_id != null){
-        $result = $db->getUserPreference($user_id);
-        if($result != null){
-            echo json_encode($result);
-        }
+        $result['data'] = $db->getUserPreference($user_id);
+        $result['success'] = 1;
     }
 }
+echo json_encode($result);
 ?>
