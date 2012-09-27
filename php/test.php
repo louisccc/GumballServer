0d2814a5df79d6a828fdb877019671b2532dcd2f
louisccc <louis29418401@gmail.com>
2012-09-27 02:30:45 -0700

    conflict

--- php/test.php
+++ php/test.php
@@ -2,7 +2,15 @@
 require_once("db.php");
 
 $db = new DB();
-echo $db->getNumberOfOnlineUser();
+if( isset($_GET["account"]) && isset($_GET["password"])){
+    $username = $_GET["account"];
+    $password = $_GET["password"];
+    $db = new DB();
+    $result = $db->verifyUser($username, $password);
+    echo json_encode($result);
+}
+
+//echo $db->getNumberOfOnlineUser();
 //echo $db->getMaxTimeStamp();
 #$db->insertFeedbackStatusByDeviceId(2, 0, "positive");
 //$db->getDeviceIdByIpAddr($_SERVER['REMOTE_ADDR']);
